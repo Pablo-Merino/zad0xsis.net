934e6681795ec1134f44aa63b90b48ddc9dcde5c
Pablo Merino <pablo.perso1995@gmail.com>
2011-12-13 16:41:06 +0100

    new blog yay

--- blog/blog.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("./blog/xml2array.php");
-require("./blog/md.php");
-
-$contents = file_get_contents('./blog/posts.xml');
-$result = xml2array($contents);
-
-$posts = $result['posts']['post'];
-?>
